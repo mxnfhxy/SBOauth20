e38e8656aaaeeb8f2d5e84ed648440e5238828c6
mxnfhxy <mxnfhxy@163.com>
2019-09-05 01:03:45 +0800

    still need to debug for static page inho.

--- spbt-mbts-swg/src/main/java/com/rszdm/spbtmbtsswg/SpbtMbtsSwgApplication.java
+++ spbt-mbts-swg/src/main/java/com/rszdm/spbtmbtsswg/SpbtMbtsSwgApplication.java
@@ -3,9 +3,11 @@ package com.rszdm.spbtmbtsswg;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan(value = "com.rszdm.spbtmbtsswg.dao")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rszdm.spbtmbtsswg.*"})
 public class SpbtMbtsSwgApplication {
 
 	public static void main(String[] args) {
